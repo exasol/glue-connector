0cc43f458148c68fb5e5dabc59fa6288303249e8
morazow <m.orazow@gmail.com>
2022-03-02 15:55:08 +0100

    Add integration test setup. Added more tests.

---
+++ src/test/java/com/exasol/glue/ittests/BaseIntegrationTest.java
@@ -0,0 +1,87 @@
+package com.exasol.glue.ittests;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import com.exasol.containers.ExasolContainer;
+import com.exasol.dbbuilder.dialects.exasol.ExasolObjectFactory;
+import com.exasol.dbbuilder.dialects.exasol.ExasolSchema;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.SparkSession;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.testcontainers.junit.jupiter.Container;
+
+public class BaseIntegrationTest {
+    private static final Logger LOGGER = Logger.getLogger(BaseIntegrationTest.class.getName());
+    private static final String DEFAULT_DOCKER_IMAGE = "7.1.6";
+
+    @Container
+    private static final ExasolContainer<? extends ExasolContainer<?>> EXASOL = new ExasolContainer<>(
+            getExasolDockerImage()).withReuse(true);
+
+    protected static Connection connection;
+    protected static ExasolObjectFactory factory;
+    protected static ExasolSchema schema;
+    protected static SparkSession spark;
+
+    @BeforeAll
+    static void beforeAll() throws SQLException {
+        EXASOL.purgeDatabase();
+        connection = EXASOL.createConnection();
+        factory = new ExasolObjectFactory(connection);
+        schema = factory.createSchema("DEFAULT_SCHEMA");
+        spark = SparkSessionProvider.getSparkSession(getSparkConf());
+    }
+
+    @AfterAll
+    static void afterAll() throws SQLException {
+        dropSchema();
+        connection.close();
+    }
+
+    public void createSchema(final String schemaName) {
+        LOGGER.fine(() -> "Creating a new schema '" + schemaName + '"');
+        dropSchema();
+        schema = factory.createSchema(schemaName);
+    }
+
+    public String getJdbcUrl() {
+        return EXASOL.getJdbcUrl();
+    }
+
+    public String getUsername() {
+        return EXASOL.getUsername();
+    }
+
+    public String getPassword() {
+        return EXASOL.getPassword();
+    }
+
+    private static void dropSchema() {
+        if (schema != null) {
+            schema.drop();
+            schema = null;
+        }
+    }
+
+    private static String getExasolDockerImage() {
+        return System.getProperty("com.exasol.dockerdb.image", DEFAULT_DOCKER_IMAGE);
+    }
+
+    private static SparkConf getSparkConf() {
+        return new SparkConf() //
+                .setMaster("local[*]") //
+                .setAppName("Tests") //
+                .set("spark.ui.enabled", "false") //
+                .set("spark.app.id", getRandomAppId()) //
+                .set("spark.driver.host", "localhost");
+    }
+
+    private static String getRandomAppId() {
+        return "SparkAppID" + (int) (Math.random() * 1000 + 1);
+    }
+
+}
---
+++ src/test/java/com/exasol/glue/ittests/SchemaInferenceIT.java
@@ -0,0 +1,92 @@
+package com.exasol.glue.ittests;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.List;
+import java.util.Map;
+
+import com.exasol.dbbuilder.dialects.Table;
+
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructType;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Tag("integration")
+@Testcontainers
+class SchemaInferenceIT extends BaseIntegrationTest {
+
+    @Test
+    void testSparkSanityCheck() {
+        final StructType schema = new StructType() //
+                .add("col_str", DataTypes.StringType, false) //
+                .add("col_int", DataTypes.IntegerType, false);
+        spark.createDataFrame(List.of(RowFactory.create("value", 10)), schema).show();
+    }
+
+    @Test
+    void testInferSchemaMultiColumn() {
+        final Table table = schema.createTable("multi_column_table", //
+                List.of("c1", "c2", "c3", "c4"), //
+                List.of("VARCHAR(10)", "INTEGER", "DATE", "DOUBLE"));
+        final StructType expectedSchema = new StructType() //
+                .add("c1", DataTypes.StringType, true) //
+                .add("c2", DataTypes.IntegerType, true) //
+                .add("c3", DataTypes.DateType, true) //
+                .add("c4", DataTypes.DoubleType, true);
+        final StructType schema = spark.read() //
+                .format("exasol") //
+                .option("query", "SELECT * FROM " + table.getFullyQualifiedName()) //
+                .options(getDefaultOptions()) //
+                .load() //
+                .schema();
+        assertThat(schema, equalTo(expectedSchema));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = { //
+            "BOOLEAN;        BOOLEAN", //
+            "INTEGER;        INT", //
+            "DOUBLE;         DOUBLE", //
+            "FLOAT;          FLOAT", //
+            "SHORTINT;       SHORT", //
+            "SMALLINT;       SHORT", //
+            "TINYINT;        SHORT", //
+            "BIGINT;         LONG", //
+            "DEC(36);        DECIMAL(36,0)", //
+            "DECIMAL;        DECIMAL(18,0)", //
+            "DECIMAL(3,2);   DECIMAL(3,2)", //
+            "DECIMAL(36,36); DECIMAL(36,36)", //
+            "CHAR(20);       STRING", //
+            "VARCHAR(20);    STRING", //
+            "CLOB;           STRING", //
+            "CLOB(20);       STRING", //
+            "DATE;           DATE", //
+            "TIMESTAMP;      TIMESTAMP" //
+    }, delimiter = ';')
+    void testInferSchemaSingleColumn(final String exasolType, final String sparkType) {
+        final StructType schema = StructType.fromDDL("c1 " + sparkType);
+        assertThat(getSingleColumnTableSchema(exasolType), equalTo(schema));
+    }
+
+    private StructType getSingleColumnTableSchema(final String exasolType) {
+        final String tableName = exasolType.replaceAll("[(,)]", "_") + "_TABLE";
+        final Table table = schema.createTable(tableName, "c1", exasolType);
+        return spark.read() //
+                .format("exasol") //
+                .option("table", table.getFullyQualifiedName()) //
+                .options(getDefaultOptions()) //
+                .load() //
+                .schema();
+    }
+
+    private Map<String, String> getDefaultOptions() {
+        return Map.of("jdbc_url", getJdbcUrl(), "username", getUsername(), "password", getPassword());
+    }
+
+}
