41dadb9d9942680cc2428dd8b08db22666b8b32c
morazow <916295+morazow@users.noreply.github.com>
2022-05-05 10:59:29 +0200

    Added s3 client for using within AWS service
    Fixes #26

--- src/main/java/com/exasol/glue/ExasolTable.java
+++ src/main/java/com/exasol/glue/ExasolTable.java
@@ -70,8 +70,7 @@ public class ExasolTable implements SupportsRead {
     }
 
     private void validateS3BucketExists(final S3ClientFactory s3ClientFactory, final String s3Bucket) {
-        final S3Client s3Client = s3ClientFactory.getS3Client();
-        try {
+        try (final S3Client s3Client = s3ClientFactory.getDefaultS3Client()) {
             s3Client.headBucket(HeadBucketRequest.builder().bucket(s3Bucket).build());
         } catch (final NoSuchBucketException exception) {
             throw new ExasolValidationException(
