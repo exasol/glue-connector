89530b9e0685c8f101f001b7e8d801859e78f1cc
morazow <916295+morazow@users.noreply.github.com>
2022-05-13 14:14:16 +0200

    Removed empty line

--- src/main/java/com/exasol/glue/ExasolTable.java
+++ src/main/java/com/exasol/glue/ExasolTable.java
@@ -112,7 +112,6 @@ public class ExasolTable implements SupportsRead {
                     .message("The number of partitions is larger than maximum allowed {{MAXPARTITIONS}} value.",
                             String.valueOf(MAX_ALLOWED_NUMBER_OF_PARTITIONS))
                     .mitigation("Please set the number of partitions parameter to lower value.").toString());
-
         }
     }
 
